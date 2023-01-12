2bfbd0d3d954f2aede592a21f952afe8ba24f3d2
Abdollah Taghavizad <ataghav@gmail.com>
2023-01-12 01:27:39 +0100

    clean up

--- dotnet/Controllers/PrimesController.cs
+++ dotnet/Controllers/PrimesController.cs
@@ -22,14 +22,12 @@ public class PrimesController : ControllerBase
 
     private IEnumerable<Int32> retainPrimes(IEnumerable<Int32> numbers)
     {
-        // _logger.LogInformation("the input has size = " + numbers.Count());
         return numbers.Where(num => isPrime(num));
 
     }
 
     private Boolean isPrime(Int32 number)
     {
-        // _logger.LogInformation("checking isPrime for number = " + number);
         var isPrime = true;
         if (number < 2)
         {
@@ -38,13 +36,11 @@ public class PrimesController : ControllerBase
         else
         {
             var count = Enumerable.Range(2, number - 2).ToList().Where(num => number % num == 0).Count();
-            // _logger.LogInformation("count = " + count);
             if (count > 0)
             {
                 isPrime = false;
             }
         }
-        // _logger.LogInformation("isPrime = " + isPrime);
         return isPrime;
     }
 }
